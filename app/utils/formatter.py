a250a490b324df383c72a4531463d4467b5f1e95
Всеволод <kreepmeister@gmail.com>
2023-11-19 13:31:40 +0300

    🪲 Fix formatter

--- app/utils/formatter.py
+++ app/utils/formatter.py
@@ -1,13 +1,15 @@
 async def format_schedule(day):
     text = ""
+    index = 0
 
     for lesson in day:
-        index = day.index(lesson) + 1
         if type(lesson) == list:
             lesson1 = lesson[0] if lesson[0] != None else "Нет пары"
             lesson2 = lesson[1] if lesson[1] != None else "Нет пары"
             text += f"{index}. {lesson1} | {lesson2}\n"
         else:
             text += f"{index}. {lesson if lesson != None else 'Нет пары'}\n"
+
+        index += 1
 
     return text
